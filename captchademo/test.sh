6bb101053400f66c2c3ab4370dddb5b03018c9cd
Sunhao <facesmall@hotmail.com>
2016-07-13 11:33:34 +0800

    l

--- captchademo/test.sh
+++
@@ -1,1 +0,0 @@
-cat tieba33.jpg | base64 | curl reniku.cn/ef22ed0d31cf2eccdfe4f2cd0ef8198b/tieba33 -d@-
--- captchademo/testC4.sh
+++
@@ -1,1 +0,0 @@
-cat tiebaC4.png | base64 | curl reniku.cn/ef22ed0d31cf2eccdfe4f2cd0ef8198b/tiebaC4 -d@-
