312b407d67c1cb324fb8f7f97a182004ada2087b
Sunhao <facesmall@hotmail.com>
2016-08-10 16:45:09 +0800

    l

---
+++ my_blog/article/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+# Create your views here.
+
+def home(request):
+        return HttpResponse("Hello World, Django")
+
+def detail(request,my_args):
+        post = Article.objects.all()[int(my_args)]
+        return HttpResponse("You're looking at my_args %s." % my_args)
