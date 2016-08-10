d326ec003e0fe3ec8581be00e5ac724655c35e65
Sunhao <facesmall@hotmail.com>
2016-08-10 18:08:30 +0800

    l

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -7,5 +7,5 @@ def home(request):
         return HttpResponse("Hello World, Django")
 
 def detail(request,my_args):
-        post = Article.objects.all()[int(my_args)]
+        # post = Article.objects.all()[int(my_args)]
         return HttpResponse("You're looking at my_args %s." % my_args)
