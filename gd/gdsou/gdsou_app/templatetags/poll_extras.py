f784d65f255c4c223f3a31a248e997d212059eab
Sunhao <facesmall@hotmail.com>
2016-09-01 18:22:26 +0800

    l

--- gd/gdsou/gdsou_app/templatetags/poll_extras.py
+++ gd/gdsou/gdsou_app/templatetags/poll_extras.py
@@ -11,6 +11,14 @@ def timeformat(value):
     seconds = (current_time-post_time).total_seconds()
     
     if seconds<3600:
-        time_str = str(seconds/60)+'分钟前'
-    elif seconds>=3600 and seconds< 86400
-        time_str = str(seconds/60/60)+'小时前'
+        time_str = str(int(seconds/60))+'分钟前'
+    elif seconds>=3600 and seconds< 86400:
+        time_str = str(int(seconds/60/60))+'小时前'
+    elif seconds>=86400 and seconds< 2073600:
+        time_str = str(int(seconds/60/60/24))+'天前'
+    elif seconds>=2073600 and seconds<62208000:
+        time_str = str(int(seconds/60/60/24/30))+'月前'
+    elif seconds>=62208000:
+        time_str = str(int(seconds/60/60/24/30/12))+'月前'
+    return time_str
+        
