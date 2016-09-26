42f736808eae0e2c72dd6eb36b9f65f82b065580
Sunhao <facesmall@hotmail.com>
2016-09-26 15:20:21 +0800

    滑动侧导航

--- gd/gdsou/gdsou_app/static/js/offcanvas.js
+++ gd/gdsou/gdsou_app/static/js/offcanvas.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
-    console.log("1111111111")
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 });
+
+$(".visible-xs button").click(function(e){
+    console.log(e);
+    button = $(e.currentTarget);
+    if(button.find('span').hasClass("glyphicon-chevron-left")){
+        button.find('span').removeClass("glyphicon-chevron-left");
+        button.find('span').addClass("glyphicon-chevron-right");
+        
+    }else{
+        button.find('span').removeClass("glyphicon-chevron-right");
+        button.find('span').addClass("glyphicon-chevron-left");
+    }
+})
